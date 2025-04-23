fcf1fe4616c5d7b06cf0b448f813a02bb5c6671d
ShimizuRyuga <oibc36164@ojs.ac.jp>
2025-04-23 11:06:30 +0900

    ヤードからメートルの変換アプリ

---
+++ Chapter02/演習問題/YardConverter/YardConverter/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace YardConverter {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("***　変換アプリ　***");
+            Console.WriteLine("1:ヤードからメートル");
+            Console.WriteLine("2:メートルからヤード");
+            Console.WriteLine("1か2を入力してください");
+            int number = int.Parse(Console.ReadLine());
+
+            if (number == 1) {
+                Console.Write("返還前(ヤード:");
+                int yard = int.Parse(Console.ReadLine());
+                PrintYardToMeterList(yard);
+            } else if (number == 2) {
+                Console.Write("返還前(メートル):");
+                int meter = int.Parse(Console.ReadLine());
+                PrintMeterToYardList(meter);
+            }
+
+            static void PrintYardToMeterList(int yard) {
+                double meter = yard * 0.9144;
+                Console.Write($"変更後(メートル):{meter}m");
+            }
+
+            static void PrintMeterToYardList(int meter) {
+                double yard = meter / 0.9144;
+                Console.Write($"変換後(ヤード):{yard}ヤード");
+
+            }
+
+
+
+
+
+
+        }
+    }
+}
