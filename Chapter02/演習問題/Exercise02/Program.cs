ad09109bd3924aa12218ab5ae2d036e269ac34a2
ShimizuRyuga <oibc36164@ojs.ac.jp>
2025-04-23 09:51:07 +0900

    変換アプリ

--- Chapter02/演習問題/Exercise02/Program.cs
+++ Chapter02/演習問題/Exercise02/Program.cs
@@ -3,13 +3,45 @@
 namespace Exercise02 {
     internal class Program {
         static void Main(string[] args) {
+            Console.WriteLine("***　変換アプリ　***");
+            Console.WriteLine("1:インチからメートル");
+            Console.WriteLine("2:メートルからインチ");
+            Console.WriteLine("1か2を入力してください");
+            int number = int.Parse(Console.ReadLine());
+            
 
-            double inchToMeter = 0.0254;
+            Console.Write("はじめ：");
+            int start = int.Parse(Console.ReadLine());
+            Console.Write("おわり：");
+            int end = int.Parse(Console.ReadLine());
 
-            //1から１０のループ
-            for (int inch = 1; inch <= 10; inch++) {
-                double meter = inch * inchToMeter;
-                Console.WriteLine($"{inch}インチ = {meter:0.0000}m");
+            if( number == 1) {
+                PrintInchToMeterList(start, end);
+            } else if(number == 2) {
+                PrintMeterToInchList(start, end);
+            } else {
+                Console.WriteLine("エラー");
+            }
+            
+
+            
+
+            //インチからメートルの1から１０のループ
+            static void PrintInchToMeterList(int start,int end) {
+                double inchToMeter = 0.0254;
+                for (int inch = start; inch <= end; inch++) {
+                    double meter = inch * inchToMeter;
+                    Console.WriteLine($"{inch}インチ = {meter:0.0000}m");
+                }
+            }
+
+            //メートルからインチの１から１０のループ
+            static void PrintMeterToInchList(int start, int end) {
+                double inchToMeter = 0.0254;
+                for (int meter = start; meter <= end; meter++) {
+                    double inch = meter / inchToMeter;
+                    Console.WriteLine($"{meter}m = {inch:0.0000}インチ");
+                }
             }
         }
     }
