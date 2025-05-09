da856fdbca27adcfa9ab86bcfba2f8234634e793
ShimizuRyuga <oibc36164@ojs.ac.jp>
2025-05-09 15:59:49 +0900

    暗黙の型指定を使用

--- Chapter02/SalesCalculator/SalesCounter.cs
+++ Chapter02/SalesCalculator/SalesCounter.cs
@@ -19,7 +19,7 @@ namespace SalesCalculator {
         //店舗別売り上げを求める
         public IDictionary<string, int> GetPerStoreSales() {
             var dict = new SortedDictionary<string, int>();
-            foreach (Sale sale in _sales) {
+            foreach (var sale in _sales) {
                 if (dict.ContainsKey(sale.ShopName))
                     dict[sale.ShopName] += sale.Amount;
                 else
@@ -30,13 +30,13 @@ namespace SalesCalculator {
         //売り上げデータを読み込み、saleオブジェクトのリストを返す
       public static IEnumerable<Sale> ReadSales(string filePath) {
             //売り上げデータを入れるリストオブジェクトを生成
-            List<Sale> sales = new List<Sale>();
+            var sales = new List<Sale>();
             //ファイルを一気に読み込み
             string[] lines = File.ReadAllLines(filePath);
             //読み込んだ行数分繰り返し
             foreach (string line in lines) {
                 string[] items = line.Split(',');
-                Sale sale = new Sale {
+               var sale = new Sale {
                     ShopName = items[0],
                     ProductCategory = items[1],
                     Amount = int.Parse(items[2])
