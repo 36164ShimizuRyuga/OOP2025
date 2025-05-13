2c26616ea8916ce8eecac30f2e2c773eb11fba3b
ShimizuRyuga <oibc36164@ojs.ac.jp>
2025-05-13 10:54:28 +0900

    2.1.2完成

--- Chapter02/演習問題/Exercise01/Song.cs
+++ Chapter02/演習問題/Exercise01/Song.cs
@@ -8,14 +8,22 @@ namespace Exercise01 {
     //2.1.1
     public class Song {
         //歌のタイトル
-        public string Title { get; set; } = String.Empty;
+        public string Title { get; set; }
         //アーティスト名
-        public string ArtistName { get; set; } = String.Empty;
+        public string ArtistName { get; set; }
         //演奏時間
-        public int Length;
+        public int Length { get; set; }
+        //演奏時間
 
+        //2.1.2
+        public Song(string title, string artistName, int length) {
+            Title = title;
+            ArtistName = artistName;
+            Length = length;
+        }
 
     }
-    //2.1.2
+   
+
 
 }
