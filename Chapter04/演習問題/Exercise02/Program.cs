5fe91def01e234dfbb7b358232381460e6ba1320
ShimizuRyuga <oibc36164@ojs.ac.jp>
2025-05-23 15:26:27 +0900

    1完成

---
+++ Chapter04/演習問題/Exercise02/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Exercise02 {
+    internal class Program {
+        static void Main(string[] args) {
+            Exercise1();
+            Console.WriteLine("---");
+            Exercise2();
+            Console.WriteLine("---");
+            Exercise3();
+        }
+
+        private static void Exercise1() {
+            var line = Console.ReadLine();
+            var num = int.Parse(line);
+            if (num < 0) {
+                Console.WriteLine(num);
+            } else if(num>=0&&num<100) {
+                Console.WriteLine(num * 2);
+            }else if (num >= 100 && num < 500) {
+                Console.WriteLine(num * 3);
+            } else {
+                Console.WriteLine(num);
+            }
+
+        }
+
+        private static void Exercise2() {
+            
+        }
+
+        private static void Exercise3() {
+            
+        }
+    }
+}
