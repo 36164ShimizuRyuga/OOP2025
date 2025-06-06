545b1836c92d5882288376e67c476ba27040b624
ShimizuRyuga <oibc36164@ojs.ac.jp>
2025-06-06 15:51:53 +0900

    一応完成

---
+++ Chapter05/Section02/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Section02 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            var appVar1 = new AppVersion(5, 1);
+            var appVar2 = new AppVersion(5, 1);
+
+            if(appVar1 == appVar2) {
+                Console.WriteLine("等しい");
+            } else {
+                Console.WriteLine("等しくない");
+            }
+        }
+    }
+
+    //プライマリーコンストラクタを使った定義
+    public record AppVersion(int m,int mi,int b=0,int r=0) {
+        public int Major { get; init; } = m;
+        public int Minor { get; init; } = mi;
+        public int Build { get; init; } = b;
+        public int Revision { get; init; } = r;
+
+      
+
+
+
+      //  public override string ToString() =>
+      //    $"{Major}.{Minor}.{Build}.{Revision}";
+
+    }
+}
---
+++ Chapter05/演習問題/Exercise01/YearMonth.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01{
+    //5.1.1
+   public class YearMonth{
+        public int Year { get; init; }
+        public int Month { get; init; }
+
+        public YearMonth(int year,int month) {
+            Year = year;
+            Month = month;
+
+        }
+        //5.1.2
+        //設定された西暦が２１世紀か判断する
+        //Yearが2001～2100年の間ならtrue,それ以外ならfalseを返す
+        public bool Is21Century => Year >= 2001 && Year <= 2100;
+
+        //5.1.3
+        public YearMonth AddOneMonth() {
+            int newYear = Year;
+            int newMonth = Month + 1;
+
+            if (newMonth > 12) {
+                newYear++;
+                newMonth = 1;
+            }
+
+            return new YearMonth(newYear, newMonth);
+        }
+        //5.1.4
+        public override string ToString() =>
+            
+        }
+    }
+}
