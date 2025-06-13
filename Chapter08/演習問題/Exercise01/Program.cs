19218c9e8d52fb8ca27f2bfc7ea73e38974f6c98
ShimizuRyuga <oibc36164@ojs.ac.jp>
2025-06-13 13:41:41 +0900

    訂正版

--- Chapter08/演習問題/Exercise01/Program.cs
+++ Chapter08/演習問題/Exercise01/Program.cs
@@ -29,14 +29,33 @@ namespace Exercise01 {
             //　登録済み：valueをインクリメント
             //未登録：valueに１を設定
             //5 2にもどる　
-            foreach (var pair in dict) {
+            foreach (var pair in dict.OrderBy(x=>x.Key)) {
                 Console.WriteLine($"{pair.Key}: {pair.Value}");
             }
 
         }
 
         private static void Exercise2(string text) {
-            
+            //1 ディクショナリインスタンスの生成
+            var dict = new SortedDictionary<char, int>();
+            //2 一文字取り出す, 3 大文字に変化
+            foreach (var c in text.ToUpper()) {
+                //4 アルファベットならディクショナリ登録
+
+                if ('A' <= c && c <= 'Z') {
+                    if (dict.ContainsKey(c)) {
+                        dict[c]++;
+                    } else {
+                        dict[c] = 1;
+                    }
+                }
+            }
+            //　登録済み：valueをインクリメント
+            //未登録：valueに１を設定
+            //5 2にもどる　
+            foreach (var pair in dict) {
+                Console.WriteLine($"{pair.Key}: {pair.Value}");
+            }
         }
     }
 }
