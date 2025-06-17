3a0d414e3c0803a34e460514967bb1ccabad03aa
ShimizuRyuga <oibc36164@ojs.ac.jp>
2025-06-17 10:14:13 +0900

    8.2.1と8.2.2完成

--- Chapter08/演習問題/Exercise02/Abbreviations.cs
+++ Chapter08/演習問題/Exercise02/Abbreviations.cs
@@ -30,4 +30,12 @@ public class Abbreviations {
             }
         }
     }
+    //8.2.1
+    public int Count =>_dict.Count ;
+
+    //8.2.2
+    public bool Remove(string abb) => _dict.Remove(abb);
+
+    //8.2.3
+
 }
