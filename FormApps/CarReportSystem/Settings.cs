fd8ee6402e64e557bf62c4d85ee8d3abfd798908
ShimizuRyuga <oibc36164@ojs.ac.jp>
2025-07-11 13:50:28 +0900

    シングルトン

--- FormApps/CarReportSystem/Settings.cs
+++ FormApps/CarReportSystem/Settings.cs
@@ -8,8 +8,23 @@ using System.Threading.Tasks;
 namespace CarReportSystem {
     [Serializable]
     public class Settings {
+
+        private static Settings instance;  //自分自身のインスタンスを格納
+        
+
+
         // 設定した色情報を格納
         public int MainFormBackColor { get; set; }
+
+        //コンストラクタ(privateにすることによりnewできなくする)
+        private Settings() { }
+
+        public static Settings getInstance() {
+            if(instance == null) {
+                instance = new Settings();
+            }
+            return instance;
+        }
     }
 }
     
