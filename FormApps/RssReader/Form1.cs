14b2072a640e533832d8b32d54351775c2d76735
ShimizuRyuga <oibc36164@ojs.ac.jp>
2025-07-22 15:58:22 +0900

    お気に入り登録

--- FormApps/RssReader/Form1.cs
+++ FormApps/RssReader/Form1.cs
@@ -2,19 +2,37 @@ using System.Net;
 using System.Security.Policy;
 using System.Windows.Forms;
 using System.Xml.Linq;
+using static System.Net.WebRequestMethods;
 
 namespace RssReader {
     public partial class Form1 : Form {
 
         private List<ItemData> items;
 
+        Dictionary<string, string> rssUrlDict = new Dictionary<string, string>() {
+            {"バスケ","https://news.yahoo.co.jp/rss/media/bballk/all.xml" },
+            {"主要","https://news.yahoo.co.jp/rss/topics/top-picks.xml" },
+            {"経済","https://news.yahoo.co.jp/rss/topics/business.xml" },
+            {"IT","https://news.yahoo.co.jp/rss/topics/it.xml" },
+            {"国内","https://news.yahoo.co.jp/rss/topics/domestic.xml" },
+            {"エンタメ","https://news.yahoo.co.jp/rss/topics/entertainment.xml" },
+
+        };
+        
         public Form1() {
             InitializeComponent();
         }
 
+
+        private void Form1_Load(object sender, EventArgs e) {
+            cbUrl.DataSource = rssUrlDict.Keys.ToList();
+            
+        }
+
+
         private async void btRssGet_Click(object sender, EventArgs e) {
             using (var hc = new HttpClient()) {
-                string xml = await hc.GetStringAsync(cbUrl.Text);
+                string xml = await hc.GetStringAsync(getRssUrl(cbUrl.Text));
                 XDocument xdoc = XDocument.Parse(xml);
 
 
@@ -29,11 +47,20 @@ namespace RssReader {
 
                 //リストボックスへタイトルを表示
                 lbTitles.Items.Clear();
-                items.ForEach(item => lbTitles.Items.Add(item.Title));
+                items.ForEach(item => lbTitles.Items.Add(item.Title ?? "データなし"));
 
             }
 
 
+        }
+
+        //コンボボックスの文字列型をチェックしてアクセス可能なURLを返す
+        private string getRssUrl(string str) {
+            if (rssUrlDict.ContainsKey(str)) {
+                return rssUrlDict[str];
+            } else {
+                return str;
+            }
         }
 
         //タイトルを選択(クリック)したときに呼ばれるイベントハンドラ
@@ -53,11 +80,9 @@ namespace RssReader {
         }
 
         private void wvRssLink_SourceChanged(object sender, Microsoft.Web.WebView2.Core.CoreWebView2SourceChangedEventArgs e) {
-
+            
         }
 
-        private void Form1_Load(object sender, EventArgs e) {
-            cbUrl.Items.Add("https://news.yahoo.co.jp/rss/media/bballk/all.xml");
-        }
+       
     }
 }
