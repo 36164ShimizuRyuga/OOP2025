bfb3524c4f4154e866a74a1b5f101574d4a2fd96
ShimizuRyuga <oibc36164@ojs.ac.jp>
2025-10-22 10:51:18 +0900

    途中経過

---
+++ SQLite/CustomerApp/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using CustomerApp.Data;
+using SQLite;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CustomerApp;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window {
+    private List<Customer> _customer = new List<Customer>();
+    public MainWindow() {
+        InitializeComponent();
+        ReadDatabase();
+        CustomerListView.ItemsSource = _customer;
+
+    }
+    public void ReadDatabase() {
+        using (var connection = new SQLiteConnection(App.databasePath)) {
+            connection.CreateTable<Customer>();
+            _customer = connection.Table<Customer>().ToList();
+        }
+    }
+
+
+    private void PictureButton_Click(object sender, RoutedEventArgs e) {
+
+    }
+
+    private void saveButton_Click(object sender, RoutedEventArgs e) {
+        var costomer = new Customer() {
+            Name = NameTextBox.Text,
+            Phone = PhoneTextBox.Text,
+            Address= AddressTextBox.Text,
+        };
+    }
+
+    private void deleteButton_Click(object sender, RoutedEventArgs e) {
+
+    }
+
+    private void updateButton_Click(object sender, RoutedEventArgs e) {
+        var selectedCustomer = CustomerListView.SelectedItem as Customer;
+        if (selectedCustomer is null) return;
+
+        using (var connection = new SQLiteConnection(App.databasePath)) {
+            connection.CreateTable<Customer>();
+
+            var person = new Customer() {
+                Name = NameTextBox.Text,
+                Phone = PhoneTextBox.Text,
+                Address = AddressTextBox.Text,
+
+            };
+            connection.Update(person);
+            ReadDatabase();
+            CustomerListView.ItemsSource = _customer;
+        }
+    }
+
+    private void CustomerListView_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+        var selectedCustomer = CustomerListView.SelectedItem as Customer;
+        if (selectedCustomer is null) return;
+        NameTextBox.Text = selectedCustomer.Name;
+        PhoneTextBox.Text = selectedCustomer.Phone;
+        AddressTextBox.Text = selectedCustomer.Address;
+    }
+
+   
+}
