463de558e3d315a79dba55b809b9cac56258d423
ShimizuRyuga <oibc36164@ojs.ac.jp>
2025-11-13 14:00:18 +0900

    ファイルパス完成

---
+++ Chapter15/TemplateMethod/LineCounter/LineCounterProcessor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TextFileProcessor;
+
+namespace LineCounter {
+    internal class LineCounterProcessor : TextProcessor{
+        private int _count = 0;
+        private string _word = "";
+
+        protected override void Initialize(string fname) {
+            _count = 0;
+            Console.WriteLine("単語を入力");
+            _word = Console.ReadLine();
+        }
+        protected override void Execute(string line) {
+            foreach (var item in line.Split(' ')) {
+                if (item.Contains(_word)) {
+                    _count++;
+                }
+            }
+        }
+
+        protected override void Terminate() => Console.WriteLine("{0}は{1}回出現しました", _word,_count);
+          
+        
+    }
+}
---
+++ Chapter15/TemplateMethod/LineCounter/Program.cs
@@ -0,0 +1,12 @@
+﻿using TextFileProcessor;
+using static System.Collections.Specialized.BitVector32;
+
+namespace LineCounter {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("ファイルパスを指定");
+            string path = Console.ReadLine();
+            TextProcessor.Run<LineCounterProcessor>(path);
+        }
+    }
+}
