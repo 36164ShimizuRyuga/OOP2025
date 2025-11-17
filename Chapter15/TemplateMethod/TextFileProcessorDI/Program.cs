22792ddfbfff359f06803267eba25d41c53aee4c
ShimizuRyuga <oibc36164@ojs.ac.jp>
2025-11-17 15:05:55 +0900

    依存性の注入

---
+++ Chapter15/TemplateMethod/TextFileProcessorDI/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TextFileProcessorDI {
+    internal class Program {
+        static void Main(string[] args) {
+           var service = new LineCounterService();
+            var prcessor =new TextFileProcessor(service);
+            Console.Write("パスに入力：");
+            prcessor.Run(Console.ReadLine());
+        }
+    }
+}
