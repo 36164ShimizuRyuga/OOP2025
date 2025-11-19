41c9f34064fd4209bb8eff123f004d23790615a4
ShimizuRyuga <oibc36164@ojs.ac.jp>
2025-11-19 10:29:29 +0900

    15.1解答

---
+++ Chapter15/TemplateMethod/TextFileProcessorDI/LineOutputService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextFileProcessorDI {
+    public class LineOutputService : ITextFileService {
+        private int _count;
+        private const int MaxLines = 20; 
+
+       
+        public void Initialize(string fname) {
+            _count = 0;
+            Console.WriteLine($"Processing file: {fname}");
+        }
+
+        // 各行をコンソールに出力（最大20行まで）
+        public void Execute(string line) {
+            if (_count < MaxLines) {
+                _count++;
+                Console.WriteLine(line);  // 行を表示
+            }
+        }
+
+       
+        public void Terminate() {
+            Console.WriteLine($"Processed {_count} lines.");
+           
+
+        }
+    }
+}
---
+++ Chapter15/TemplateMethod/TextFileProcessorDI/LineToHalfNumberService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextFileProcessorDI {
+    public class LineToHalfNumberService : ITextFileService {
+
+
+
+        public void Initialize(string fileName) {
+
+        }
+        public void Execute(string line) {
+            string result = new string(
+                line.Select(c => ('０' <= c && c <= '９') ? (char)(c - '０' + '0') : c).ToArray());
+
+            Console.WriteLine(result);
+        }
+
+
+        public void Terminate() {
+
+        }
+    }
+}
